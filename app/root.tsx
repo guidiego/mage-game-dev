aeeddd8a2955ef9596afe92783c796ecaa79433b
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2022-03-29 17:56:37 +0200

    initial

---
+++ app/root.tsx
@@ -0,0 +1,36 @@
+import styles from "./tailwind.css";
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import type { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+  return { title: "Mage Game Dev" };
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];;
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
